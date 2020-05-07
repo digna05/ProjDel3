c968d39aac2c21ae6eeacc7aafc0ba8053b589e4
digna05 <51842538+digna05@users.noreply.github.com>
2020-05-07 16:25:33 -0400

    Add files via upload

---
+++ output.py
@@ -0,0 +1,207 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:/Users/Digna/Documents/SpringSem2020/CS331/ProjectDel3/pshop.py ==
+Enter 1-14 to run the corresponding query. Enter q to quit the program: 1
+bryant04
+townsend05
+palmer08
+rodgers11
+Enter 1-14 to run the corresponding query. Enter q to quit the program: 2
+48
+54
+136
+231
+Enter 1-14 to run the corresponding query. Enter q to quit the program: 3
+Please enter a model's name: Derrick Brewer
+payne14
+Enter 1-14 to run the corresponding query. Enter q to quit the program: 4
+Corey Underwood
+Lisa Gonzalez
+Lisa Gonzalez
+Jordan Almasi
+Enter 1-14 to run the corresponding query. Enter q to quit the program: 5
+Jerald Graham
+Jordan Almasi
+Corey Underwood
+Enter 1-14 to run the corresponding query. Enter q to quit the program: 6
+TransID  3 contains  4  photos.
+Enter 1-14 to run the corresponding query. Enter q to quit the program: 7
+Please enter the photograhper's name: Nelson Hunter
+Rebecca Sparks was photgraphed by Nelson Hunter
+Kristin Drake was photgraphed by Nelson Hunter
+Enter 1-14 to run the corresponding query. Enter q to quit the program: 8
+Kadie Pugh has a combined price of $150
+Edgar Tran has a combined price of $110
+Nelson Hunter has a combined price of $95
+Sierra Vaughn has a combined price of $65
+Salvatore Terry has a combined price of $60
+Darla Pena has a combined price of $45
+Jordan Almasi has a combined price of $40
+Anna Prentice has a combined price of $35
+Jerald Graham has a combined price of $30
+Sara Chambers has a combined price of $30
+Rohan Mathis has a combined price of $25
+Amelia Kemp has a combined price of $20
+Corey Underwood has a combined price of $20
+Lisa Gonzalez has a combined price of $20
+Matt Gill has a combined price of $15
+Enter 1-14 to run the corresponding query. Enter q to quit the program: 11
+bryant04 has spent $120.99
+butler12 has spent $90.62
+griffin15 has spent $55.23
+herrera01 has spent $29.99
+larson09 has spent $70.50
+maxwell10 has spent $55.37
+palmer08 has spent $115.76
+parsons13 has spent $64.81
+payne14 has spent $72.11
+rodgers11 has spent $134.21
+sherman07 has spent $49.99
+sullivan06 has spent $30.59
+swanson02 has spent $45.00
+taylor03 has spent $80.75
+townsend05 has spent $103.50
+Enter 1-14 to run the corresponding query. Enter q to quit the program: 12
+Darla Pena has made $60.58
+Corey Underwood has made $120.99
+Anna Prentice has made $80.75
+Kadie Pugh has made $259.96
+Nelson Hunter has made $216.87
+Salvatore Terry has made $120.18
+Amelia Kemp has made $29.99
+Sara Chambers has made $142.61
+Matt Gill has made $103.50
+Sierra Vaughn has made $72.11
+Jordan Almasi has made $49.99
+Edgar Tran has made $105.36
+Jerald Graham has made $30.59
+Enter 1-14 to run the corresponding query. Enter q to quit the program: 13
+landscape sales are $465.69
+abstract sales are $271.13
+portrait sales are $439.79
+Enter 1-14 to run the corresponding query. Enter q to quit the program: 14
+2019-12-09 made $134.21 in sales.
+2019-07-28 made $120.99 in sales.
+2019-11-12 made $115.76 in sales.
+2019-09-06 made $103.50 in sales.
+2019-11-16 made $100.49 in sales.
+2020-02-14 made $90.62 in sales.
+2019-07-10 made $80.75 in sales.
+2020-03-22 made $72.11 in sales.
+2020-02-29 made $64.81 in sales.
+2019-12-01 made $55.37 in sales.
+2020-04-04 made $55.23 in sales.
+2019-10-29 made $49.99 in sales.
+2019-05-15 made $45.00 in sales.
+2019-10-19 made $30.59 in sales.
+Enter 1-14 to run the corresponding query. Enter q to quit the program: 9
+Enter the photoID of the photo to be deleted: 54
+PhotoIDs before deleting: 
+20
+25
+37
+48
+52
+54
+67
+68
+70
+75
+82
+103
+121
+129
+136
+141
+143
+181
+189
+193
+205
+218
+231
+288
+292
+The photo with PhotoID: 54 was deleted.
+PhotoIDs after deleting: 
+20
+25
+37
+48
+52
+67
+68
+70
+75
+82
+103
+121
+129
+136
+141
+143
+181
+189
+193
+205
+218
+231
+288
+292
+Enter 1-14 to run the corresponding query. Enter q to quit the program: 10
+Enter the photoID of the photo to be updated: 136
+Enter the name to update to: Sara Chambers
+PhotoIDs and Photographers before updating: 
+20 , Darla Pena
+25 , Corey Underwood
+37 , Anna Prentice
+48 , Sierra Vaughn
+52 , Kadie Pugh
+67 , Nelson Hunter
+68 , Nelson Hunter
+70 , Salvatore Terry
+75 , Salvatore Terry
+82 , Amelia Kemp
+103 , Nelson Hunter
+121 , Sara Chambers
+129 , Matt Gill
+136 , Lisa Gonzalez
+141 , Darla Pena
+143 , Sierra Vaughn
+181 , Jordan Almasi
+189 , Edgar Tran
+193 , Sara Chambers
+205 , Kadie Pugh
+218 , Kadie Pugh
+231 , Edgar Tran
+288 , Edgar Tran
+292 , Jerald Graham
+The photo with the PhotoID 136 had the photograhper name updated to Sara Chambers.
+PhotoIDs and Photographers after updating: 
+20 , Darla Pena
+25 , Corey Underwood
+37 , Anna Prentice
+48 , Sierra Vaughn
+52 , Kadie Pugh
+67 , Nelson Hunter
+68 , Nelson Hunter
+70 , Salvatore Terry
+75 , Salvatore Terry
+82 , Amelia Kemp
+103 , Nelson Hunter
+121 , Sara Chambers
+129 , Matt Gill
+136 , Sara Chambers
+141 , Darla Pena
+143 , Sierra Vaughn
+181 , Jordan Almasi
+189 , Edgar Tran
+193 , Sara Chambers
+205 , Kadie Pugh
+218 , Kadie Pugh
+231 , Edgar Tran
+288 , Edgar Tran
+292 , Jerald Graham
+Enter 1-14 to run the corresponding query. Enter q to quit the program: q
+>>> 
